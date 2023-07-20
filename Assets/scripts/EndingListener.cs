b21e9b7eaa2e513fa388a28b224aad0e2eda3897
Erik Schröder <erikschroeder2002@gmail.com>
2023-07-20 14:52:26 +0200

    added lose game event

--- Assets/scripts/EndingListener.cs
+++ Assets/scripts/EndingListener.cs
@@ -7,21 +7,27 @@ public class EndingListener : MonoBehaviour
     
 
     public static Vector3 penguSpeed;
-    public static int /*Datentyp ändern falls nötig*/ penguHeight;
+    public static int /*Datentyp ändern falls nötig*/ penguHeight = 3;
 
-    public GameObject gameOverText;
+    public GameObject winText;
+    public GameObject loseText;
     public GameObject restartButton;
     public GameObject menuButton;
 
-    private bool gameEnded = false;
+    private bool gameWon = false;
+    private bool gameLost = false;
+
+    private float endTextY = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameOverText = GameObject.Find("EndText");
+        winText = GameObject.Find("WinText");
+        loseText = GameObject.Find("LoseText");
         menuButton = GameObject.Find("MenuButton");
         restartButton = GameObject.Find("RestartButton");
-        gameOverText.SetActive(false);
+        winText.SetActive(false);
+        loseText.SetActive(false);
         menuButton.SetActive(false);
         restartButton.SetActive(false);
     }
@@ -29,22 +35,32 @@ public class EndingListener : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(penguSpeed.magnitude >= 11200 /* m/s */ || penguHeight >= 5){
-            EndGame();
+        if((penguSpeed.magnitude >= 11200 /* m/s */ || penguHeight >= 5) && !gameLost){
+            WinGame();
         }
-        if(gameEnded){
-            gameOverText.SetActive(true);
-//            Debug.Log(text.transform.position);
-            if(gameOverText.transform.localPosition.y <= 10f){
-                menuButton.SetActive(true);
-                restartButton.SetActive(true);
-                Time.timeScale = 0f;
-            }
+        else if(penguHeight <= 0 && !gameWon){
+            LoseGame();
+        }
+
+        if(gameWon){
+            winText.SetActive(true);
         } 
+        else if(gameLost){
+            loseText.SetActive(true);
+        } 
+
+        if(winText.transform.localPosition.y <= endTextY || loseText.transform.localPosition.y <= endTextY){
+            menuButton.SetActive(true);
+            restartButton.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
 
-    public void EndGame(){
-        gameEnded = true;
-        Debugger.isPaused = true;       
+    public void WinGame(){
+        gameWon = true;
+    }
+
+    public void LoseGame(){
+        gameLost = true;
     }
 }
